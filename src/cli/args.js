3f57f55bc6472a9f277ef7f79eb4e1eba7705eac
Intrstng <intrstg@gmail.com>
2024-01-23 18:33:37 +0300

    feat: complete Command line interface(src/cli) task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+    let result = '';
+    const envArguments = process.argv.slice(2);
+
+    for (let i = 0; i < envArguments.length - 1; i += 2) {
+        result += `${envArguments[i]} is ${envArguments[i+1]}, `;
+    }
+
+    console.log(result.slice(0,-2));
 };
 
 parseArgs();

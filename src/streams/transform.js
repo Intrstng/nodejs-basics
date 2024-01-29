a7b2bc0a12301fa79e2b94552de35160612eebbd
Intrstng <intrstg@gmail.com>
2024-01-29 10:49:09 +0300

    refactor: add newline in the end of transformedChunk in Streams transform task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,6 @@
 import {Transform} from 'stream';
 import process from 'node:process';
+import os from 'os';
 
 const transform = async () => {
     const reverseTransform = new Transform({
@@ -7,12 +8,25 @@ const transform = async () => {
                   encoding,
                   callback
         ) {
-            const reversedChunk = chunk.toString().split('').reverse().join('');
-            this.push(reversedChunk);
+            const reversedChunk = chunk.toString().trim().split('').reverse().join('');
+            const transformedChunk = reversedChunk + '\n';
+            // const transformedChunk = reversedChunk + os.eol;
+            this.push(transformedChunk);
             callback();
-            //callback(null, chunk); или это
+            //callback(null, transformedChunk); или это
         },
     });
+
+    reverseTransform.on('error', (error) => {
+        console.error('An error occurred during text reversal:', error);
+    });
+    process.stdin.on('error', (error) => {
+        console.error('An error occurred while reading input:', error);
+    });
+    process.stdout.on('error', (error) => {
+        console.error('An error occurred while writing output:', error);
+    });
+
     process.stdin.pipe(reverseTransform).pipe(process.stdout);
 };
 
